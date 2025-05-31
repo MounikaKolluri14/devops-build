4a15bc425d2a5bf4404716f66e1f13fc9e422a12
Ubuntu <ubuntu@ip-172-31-88-135.ec2.internal>
2025-05-31 13:32:18 +0000

    updates.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e
 
 echo "Stopping existing container(s) if any..."
+docker rm -f react-production-app || true
+
 docker-compose down
 
 echo "Starting container(s)..."
 docker-compose up -d
 
 echo "Deployment complete. The app should now be running on port 80."
+
 
